be0fa619008b2bcaf0de4bdbd9f57e1013ffdf75
ANTHONY <anthonychukwudiDev@outlook.com>
2024-04-28 21:08:45 -0300

    bug-fix

--- js/script.js
+++ js/script.js
@@ -18,11 +18,11 @@ calculate = () => {
 };
 
 const showTipAmount = () => {
-    // Get the snackbar DIV
+    
     var x = document.querySelector("#tip-amount");
-    // Add the "show" class to DIV
+ 
     x.className = "show";
-    // After 3 seconds, remove the show class from DIV
+    
     setTimeout(function () {
         x.className = x.className.replace("show", "");
     }, 3000);
